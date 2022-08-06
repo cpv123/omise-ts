c8d5ccbe6c395b75739eac63c6db2f01e19a6b77
Chris Vibert <cp.vibert@gmail.com>
2022-08-06 22:12:56 +0700

    Show usage with example app

--- example-app/pages/api/charges.ts
+++ example-app/pages/api/charges.ts
@@ -1,46 +1,47 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import OmiseClient from "../../../build";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import OmiseClient from '../../../build/src/index'
 
 type Data = {
-  success: boolean;
-  message?: string;
+  success: boolean
+  message?: string
   data?: {
-    id: string;
-  };
-};
+    id: string
+  }
+}
 
 interface RequestWithBody extends NextApiRequest {
   body: {
-    amount: number;
-  };
+    amount: number
+    customerId: string
+  }
 }
 
 export default async function handler(
   req: RequestWithBody,
   res: NextApiResponse<Data>
 ) {
-  if (req.method !== "POST") {
+  if (req.method !== 'POST') {
     return res.status(500).send({
       success: false,
-      message: "Unsupported HTTP method, this API only accepts POST requests",
-    });
+      message: 'Unsupported HTTP method, this API only accepts POST requests',
+    })
   }
 
-  const { amount } = req.body;
-  const amountToCharge = amount * 100;
+  const { amount, customerId } = req.body
+  const amountToCharge = amount * 100
 
   const Omise = new OmiseClient({
     apiSecretKey: process.env.OMISE_SECRET_KEY as string,
-  });
+  })
 
   const charge = await Omise.charges.create({
     amount: amountToCharge,
-    currency: "THB",
-    customer: "***",
-  });
+    customer: customerId,
+    currency: 'THB',
+  })
 
   return res.status(200).json({
     success: true,
     data: { id: charge.id },
-  });
+  })
 }
--- example-app/pages/index.tsx
+++ example-app/pages/index.tsx
@@ -1,29 +1,50 @@
-import { useState } from "react";
-import type { NextPage } from "next";
+import { useState } from 'react'
+import type { GetStaticProps } from 'next'
+import Omise from '../../build/src/index'
+import * as Types from '../../build/types'
 
-const Home: NextPage = () => {
-  const [loading, setLoading] = useState(false);
-  const [amount, setAmount] = useState<string>("");
+type Props = {
+  customers: Types.Customers.ICustomerList['data']
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const omise = new Omise({
+    apiSecretKey: process.env.OMISE_SECRET_KEY as string,
+  })
+  const customersList = await omise.customers.list()
+  return {
+    props: {
+      customers: customersList?.data || [],
+    },
+  }
+}
+
+export default function Home({ customers }: Props) {
+  const [loading, setLoading] = useState(false)
+  const [amount, setAmount] = useState<string>('')
+  const [customerId, setCustomerId] = useState('')
 
   const handleCreateCharge = async () => {
-    setLoading(true);
-    await fetch("/api/charges", {
-      method: "POST",
+    setLoading(true)
+    await fetch('/api/charges', {
+      method: 'POST',
       body: JSON.stringify({
         amount: Number(amount),
+        customerId,
       }),
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
-    });
-    setAmount("");
-    setLoading(false);
-  };
+    })
+    setAmount('')
+    setCustomerId('')
+    setLoading(false)
+  }
 
   return (
     <div className="flex flex-col items-center p-10">
       <h1 className="text-xl font-bold mb-6">Create a charge with Omise</h1>
-      <div className="flex flex-col sm:flex-row">
+      <div className="flex flex-col sm:flex-row mb-10">
         <input
           className="mb-6 sm:mb-0 mr-4 bg-gray-50 border border-gray-300 text-gray-900 rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full px-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Amount (THB)"
@@ -31,16 +52,37 @@ const Home: NextPage = () => {
           onChange={(event) => setAmount(event.target.value)}
         />
         <button
-          disabled={loading || !amount}
+          disabled={loading || !amount || !customerId}
           onClick={handleCreateCharge}
           type="button"
-          className="w-60 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          className="w-60 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 disabled:opacity-50 disabled:cursor-not-allowed"
         >
           Create Charge
         </button>
       </div>
+      <table className="table-auto">
+        <tbody>
+          {customers?.slice(0, 3)?.map((customer) => (
+            <tr key={customer.id}>
+              <td>
+                <input
+                  type="checkbox"
+                  value={customer.id}
+                  checked={customer.id === customerId}
+                  onChange={() => setCustomerId(customer.id)}
+                  className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+              </td>
+              <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                {customer.id}
+              </td>
+              <td className="border-b border-slate-100 dark:border-slate-700 p-4 pl-8 text-slate-500 dark:text-slate-400">
+                {customer.email}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
-};
-
-export default Home;
+  )
+}
